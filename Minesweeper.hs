f53417feec92caed0ffb60c4bebdc5e4f13dfd23
Alexander Selmanovic <alesel@student.chalmers.se>
2018-12-11 10:39:50 +0100

    Update Minesweeper.hs

--- Minesweeper.hs
+++ Minesweeper.hs
@@ -9,12 +9,57 @@ import Test.QuickCheck
 data Minesweeper = Minesweeper {rows :: [[Maybe Int]]}
         deriving (Show, Eq)
 
+
+example :: Minesweeper
+example = Minesweeper [[n  ,n  ,j 1,j 0,j 1,n  ,j 1,j 0,j 0,j 0 ],
+                       [j 2,j 2,j 1,j 0,j 1,j 1,j 1,j 0,j 0,j 0 ],
+                       [j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0 ],
+                       [j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0,j 0 ],
+                       [j 1,j 1,j 1,j 0,j 0,j 0,j 0,j 0,j 0,j 0 ],
+                       [j 1,n  ,j 2,j 1,j 1,j 0,j 0,j 0,j 0,j 0 ],
+                       [j 1,j 1,j 2,n  ,j 1,j 0,j 0,j 0,j 0,j 0 ],
+                       [j 0,j 0,j 1,j 1,j 2,j 1,j 1,j 0,j 0,j 0 ],
+                       [j 0,j 0,j 0,j 0,j 1,n  ,j 1,j 1,j 1,j 1 ],
+                       [j 0,j 0,j 0,j 0,j 1,j 1,j 1,j 1,n  ,j 1 ]]
+ where
+   n = Nothing
+   j = Just
+
+printMinesweeper :: Minesweeper -> IO ()
+printMinesweeper minesweeper = putStrLn (unlines (map makeAString (rows minesweeper)))
+
+makeAString :: [Maybe Int] -> [Char]
+makeAString row = map makeChar row
+
+-- turns the elements into characters
+makeChar :: Maybe Int -> Char
+makeChar Nothing = 'X'
+makeChar (Just n) = intToDigit n
+
+
 allBlankMinesweeper :: Minesweeper
 allBlankMinesweeper = Minesweeper (replicate 10 (replicate 10 (Just 0)))
 
+
+
+
+
+
+
+
+
+
+
+
 bombMaker :: StdGen -> [Int] -> [Int]
-bombMaker g list | length list == 10 = list 
-                 | otherwise = bombMaker g' (nub (list ++ number))
-                        where (number, g') = randomR (0, 99) g
+bombMaker g list | length list == 10 = list
+                 | otherwise = bombMaker g' newList
+                    where (number, g') = randomR (0, 99) g
+                          newList = nub (list ++ [number])
+
+
+{-runGame = do putStrLn "Welcome to the Game"
+             g <- newStdGen
+             bombMaker g list-}
 
 --createMinesweeper
